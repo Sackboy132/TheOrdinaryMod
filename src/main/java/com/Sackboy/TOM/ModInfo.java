d69a5d63dd66b052afdf1c2cad726f0e110e98c8
Terry <terryyyd@gmail.com>
2016-03-28 23:47:29 +0100

    Updated the main class and added ModInfo
    Changed the main class to have 3 events, also using the new ModInfo
    class.

---
+++ src/main/java/com/Sackboy/TOM/ModInfo.java
@@ -0,0 +1,9 @@
+package com.Sackboy.TOM;
+
+public class ModInfo {
+
+	public static final String ID = "TOM";
+	public static final String NAME = "TheOrdinaryMod";
+	public static final String VERSION = "1.0";
+	
+}
--- src/main/java/com/Sackboy/TOM/TOM.java
+++ src/main/java/com/Sackboy/TOM/TOM.java
@@ -1,20 +1,28 @@
 package com.Sackboy.TOM;
 
-import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = TOM.MODID, version = TOM.VERSION)
-public class TOM
-{
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
-    
-    @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-		// some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
-    }
+@Mod(modid = ModInfo.ID, name = ModInfo.NAME, version = ModInfo.VERSION)
+
+public class TOM {
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+
+	}
+
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+
+	}
+
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
+
+	}
+	
 }
