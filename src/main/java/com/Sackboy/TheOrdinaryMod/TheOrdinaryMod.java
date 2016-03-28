484b5ec88815ea0f08e0fb2ba76dfcf36e81d472
Terry <terryyyd@gmail.com>
2016-03-28 23:38:13 +0100

    Rename of the Example :+1:
    Yay!

--- src/main/java/com/Sackboy/TheOrdinaryMod/TheOrdinaryMod.java
+++ src/main/java/com/Sackboy/TheOrdinaryMod/TheOrdinaryMod.java
@@ -1,12 +1,12 @@
-package com.example.examplemod;
+package com.Sackboy.TheOrdinaryMod;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = TheOrdinaryMod.MODID, version = TheOrdinaryMod.VERSION)
+public class TheOrdinaryMod
 {
     public static final String MODID = "examplemod";
     public static final String VERSION = "1.0";
