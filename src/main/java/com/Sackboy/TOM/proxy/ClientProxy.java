0d63196ba4c75868910a9a602eb6eed429e9eefe
Sackboy132 <aaronwsfranklin@gmail.com>
2016-04-09 22:02:03 +0100

    Github i think

--- src/main/java/com/Sackboy/TOM/proxy/ClientProxy.java
+++ src/main/java/com/Sackboy/TOM/proxy/ClientProxy.java
@@ -1,12 +1,9 @@
 package com.Sackboy.TOM.proxy;
 
+import com.Sackboy.TOM.Mob.EntityJoiter;
+import com.Sackboy.TOM.Model.ModelJoiter;
+import com.Sackboy.TOM.Render.RenderJoiter;
 import com.Sackboy.TOM.init.TOMItems;
-import com.Sackboy.TOM.mob.EntityCrawler;
-import com.Sackboy.TOM.mob.EntityJoiter;
-import com.Sackboy.TOM.model.ModelCrawler;
-import com.Sackboy.TOM.model.ModelJoiter;
-import com.Sackboy.TOM.render.RenderCrawler;
-import com.Sackboy.TOM.render.RenderJoiter;
 
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
@@ -17,8 +14,8 @@ public class ClientProxy extends CommonProxy {
 	public void registerRenders() {
 		TOMItems.registerRenders();
 
-		RenderingRegistry.registerEntityRenderingHandler(EntityCrawler.class,
-				new RenderCrawler(Minecraft.getMinecraft().getRenderManager(), new ModelCrawler(), 0f));
+		//RenderingRegistry.registerEntityRenderingHandler(EntityCrawler.class,
+			//	new RenderCrawler(Minecraft.getMinecraft().getRenderManager(), new ModelCrawler(), 0f));
 		RenderingRegistry.registerEntityRenderingHandler(EntityJoiter.class,
 				new RenderJoiter(Minecraft.getMinecraft().getRenderManager(), new ModelJoiter(), 0f));
 	}
