4feed8655ec3920582caf829faa99a5431a58b97
Terry <terryyyd@gmail.com>
2016-04-10 11:35:45 +0100

    Sneaking :snake:
    While sneaking with the armour, you can go invisible :3

--- src/main/java/com/Sackboy/TOM/item/ItemCardboardBox.java
+++ src/main/java/com/Sackboy/TOM/item/ItemCardboardBox.java
@@ -1,15 +1,26 @@
 package com.Sackboy.TOM.item;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.World;
 
 public class ItemCardboardBox extends ItemArmor {
 
 	public ItemCardboardBox(ArmorMaterial material, int renderIndex, EntityEquipmentSlot equipmentSlot) {
 		super(material, renderIndex, equipmentSlot);
 	}
-	
-	
+
+	@Override
+	public void onArmorTick(World world, EntityPlayer player, ItemStack itemStack) {
+		super.onArmorTick(world, player, itemStack);
+
+		if (player.isSneaking()) {
+			player.addPotionEffect(new PotionEffect(Potion.getPotionById(1), 100000, 1));
+		}
+	}
 }
